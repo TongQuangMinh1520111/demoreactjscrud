a634fb2ee747a2c7dfe261203c4b2d713e0e0101
MINH_VN <tqminh@b-create.jp>
2023-01-10 08:35:24 +0700

    fix

---
+++ reactcrud/src/modules/account.module.js
@@ -0,0 +1,71 @@
+'use strict';
+import axios from "axios";
+import Cookies from "universal-cookie";
+
+const AccountModule = {
+  login: async(user)=> {
+    try {
+      let login = await axios.post("http://localhost:5000/api/login", user);
+      if( login ) {
+        // check success hay ko
+        // return login.data.user
+        const cookies = new Cookies();
+        cookies.set("TOKEN", login.data.token, {
+          path: "/",
+        });
+        localStorage.setItem('token', login.data.token)
+        console.log(login.data.user)
+    
+        sessionStorage.setItem("user", JSON.stringify(login.data.user));
+        // var obj = JSON.parse(sessionStorage.getItem('user')); 
+        // console.log(obj.email);
+        // window.location.href = "/auth";
+        return {
+          success: true
+        }
+      } else {
+        return {
+          success: false,
+          errMsg: "Login khoong thanh cong "
+        }
+      }
+    } catch (err) {
+      console.log(err)
+      return {
+        success: false,
+        errMsg: err.message
+      }
+    }
+  },
+
+  register: async(user)=> {
+
+    /**
+     * return { success }
+     */
+    try {
+
+    } catch (err) {
+
+    }
+  },
+
+  logout: async()=>{
+    const cookies = new Cookies();
+    cookies.remove("TOKEN", { path: "/" });
+    window.localStorage.clear()
+    window.location.href = "/login";
+  },
+
+  verifyToken: async(token) => {
+    try {
+      // gọi api kiểm tra token họp lêj
+      // set user vao session store
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+}
+
+export default AccountModule;
